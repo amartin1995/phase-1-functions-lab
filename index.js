7fde681d98bbf80d93cb1f2aaaa31bf59d5700ee
Alex Martin <alex.pmartin1@gmail.com>
2021-09-24 16:32:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let blockValue;
+
+let distanceFromHqInBlocks = function(blockValue){
+    return Math.abs(blockValue - 42)
+}
+
+let distanceFromHqInFeet = function(blockValue){
+    return Math.abs(blockValue - 42) * 264;
+}
+
+let distanceTravelledInFeet = function(begBlock, destBlock){
+    return Math.abs(destBlock - begBlock) * 264;
+}
+
+let calculatesFarePrice = function(begBlock, destBlock){
+    if ((Math.abs(destBlock - begBlock) * 264) >= 0 && (Math.abs(destBlock - begBlock) * 264) <= 400){
+        return 0;
+    } else if (Math.abs(destBlock - begBlock) * 264 >= 401 && Math.abs(destBlock - begBlock) * 264 <= 2000){
+        return ((Math.abs(destBlock - begBlock) * 264) - 400) * 0.02;
+    } else if (Math.abs(destBlock - begBlock) * 264 > 2000 && Math.abs(destBlock - begBlock) * 264 < 2500){
+        return 25;
+    } else if (Math.abs(destBlock - begBlock) * 264 >= 2500){
+        return 'cannot travel that far'
+    }
+}
